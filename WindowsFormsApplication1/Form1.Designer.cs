a8d0250bbd23b3e742759aa03aa4155dd1bcc5a4
jiangtengfang <tengfangjiang@126.com>
2018-06-25 13:57:03 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.lv_print_history = new System.Windows.Forms.ListView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_age = new System.Windows.Forms.TextBox();
+            this.tb_sex = new System.Windows.Forms.TextBox();
+            this.tb_serial = new System.Windows.Forms.TextBox();
+            this.tb_card_id = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 428);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_print
+            // 
+            this.btn_print.Location = new System.Drawing.Point(32, 151);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(75, 23);
+            this.btn_print.TabIndex = 3;
+            this.btn_print.Text = "打印";
+            this.btn_print.UseVisualStyleBackColor = true;
+            this.btn_print.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lv_print_history
+            // 
+            this.lv_print_history.Location = new System.Drawing.Point(284, 33);
+            this.lv_print_history.Name = "lv_print_history";
+            this.lv_print_history.Size = new System.Drawing.Size(231, 436);
+            this.lv_print_history.TabIndex = 4;
+            this.lv_print_history.UseCompatibleStateImageBehavior = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 472);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(990, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Location = new System.Drawing.Point(559, 33);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(29, 12);
+            this.label_name.TabIndex = 6;
+            this.label_name.Text = "姓名";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(559, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "年龄";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(559, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "流水号";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(559, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "性别";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(559, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "身份证号";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(619, 33);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(359, 21);
+            this.tb_name.TabIndex = 11;
+            // 
+            // tb_age
+            // 
+            this.tb_age.Location = new System.Drawing.Point(619, 70);
+            this.tb_age.Name = "tb_age";
+            this.tb_age.Size = new System.Drawing.Size(359, 21);
+            this.tb_age.TabIndex = 11;
+            // 
+            // tb_sex
+            // 
+            this.tb_sex.Location = new System.Drawing.Point(619, 113);
+            this.tb_sex.Name = "tb_sex";
+            this.tb_sex.Size = new System.Drawing.Size(359, 21);
+            this.tb_sex.TabIndex = 11;
+            // 
+            // tb_serial
+            // 
+            this.tb_serial.Location = new System.Drawing.Point(619, 156);
+            this.tb_serial.Name = "tb_serial";
+            this.tb_serial.Size = new System.Drawing.Size(359, 21);
+            this.tb_serial.TabIndex = 11;
+            // 
+            // tb_card_id
+            // 
+            this.tb_card_id.Location = new System.Drawing.Point(619, 199);
+            this.tb_card_id.Name = "tb_card_id";
+            this.tb_card_id.Size = new System.Drawing.Size(359, 21);
+            this.tb_card_id.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 494);
+            this.Controls.Add(this.tb_card_id);
+            this.Controls.Add(this.tb_serial);
+            this.Controls.Add(this.tb_sex);
+            this.Controls.Add(this.tb_age);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lv_print_history);
+            this.Controls.Add(this.btn_print);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.ListView lv_print_history;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_age;
+        private System.Windows.Forms.TextBox tb_sex;
+        private System.Windows.Forms.TextBox tb_serial;
+        private System.Windows.Forms.TextBox tb_card_id;
+    }
+}
+
